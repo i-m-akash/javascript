5878794d3d3fcc5a81cc7106c29ac5823e5775dc
Akash kumar <109230614+i-m-akash@users.noreply.github.com>
2023-12-12 21:42:42 +0530

    Update 01_var.js

--- 01_basics/01_var.js
+++ 01_basics/01_var.js
@@ -1,5 +1,17 @@
-const accountId =144453
-let=accountEmail ="akashkumar.nit.er@gmail.com"
-var accounCity = "rudrapur"
-//accountId = 2
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Rudrapur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "akashkumar.nit.er@gmail.com"
+accountPassword = "21155222222233"
+accountCity = "Rudrapur"
+
 console.log(accountId);
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
